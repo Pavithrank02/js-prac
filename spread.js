7e06907262dd2aa47ec8c11f991585ace3681845
Pavithran2 <pavithrank02@gmail.com>
2023-08-08 09:05:00 +0530

    spread operator

---
+++ spread.js
@@ -0,0 +1,33 @@
+/****Default parameter**** */
+
+function fn(p1, p2, p3="default") {
+  console.log("hi params are", p1, p2, p3)
+}
+
+fn("one", "two", "three")
+fn("one", "two")
+
+
+
+/***spread operator */
+
+// let arr=[1,2,3,4,5]
+// let arr2=arr
+
+// arr2.pop()
+// arr2.push(100)
+
+// arr2[2] =200
+// console.log("contest arr", arr)
+
+
+// let arr=[1,2,[3,4],4,5]
+// let arr2=[...arr]
+
+// arr2.pop()
+// arr2.push(100)
+// arr2[2][0] =200
+// console.log("contest arr", arr)
+// console.log("contest arr", arr2)
+
+
