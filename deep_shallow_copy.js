0e9028406663c108ca56c75bd32e2b4365dfddd3
Pavithran2 <pavithrank02@gmail.com>
2023-08-10 19:10:04 +0530

    shallow copy operation

--- deep_shallow_copy.js
+++ deep_shallow_copy.js
@@ -1,6 +1,18 @@
-let arr = [1,2,3,4,5]
+// let arr = [1,2,3,4,5]
+// arr.slice(1)
 
-let arr2 = arr.splice(1, 5)
+// let arr2 = arr.splice(1)
 
-console.log(arr)
-console.log(arr2)
+// console.log(arr)
+// console.log(arr2)
+
+
+/***shallow copy */
+
+let arr = [1,2,3,4,[10,12], 4, 5]
+
+let spreadArr = [...arr]
+
+spreadArr[2] = 100
+
+console.log("sa",spreadArr, "arr",arr)
