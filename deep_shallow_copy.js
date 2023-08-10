fed29f9051a1ba7b26789dbe12873a0ed7128567
Pavithran2 <pavithrank02@gmail.com>
2023-08-10 19:24:51 +0530

    deep copy operation

--- deep_shallow_copy.js
+++ deep_shallow_copy.js
@@ -9,11 +9,35 @@
 
 /***shallow copy */
 
-let arr = [1,2,3,4,[10,12], 4, 5]
+// let arr = [1,2,3,4,[10,12], 4, 5]
 
-let spreadArr = [...arr]
+// let spreadArr = [...arr]
 
-spreadArr[2] = 100
-spreadArr[4][1] = 200
+// spreadArr[2] = 100
+// spreadArr[4][1] = 200
 
-console.log("sa",spreadArr, "arr",arr)
+// console.log("sa",spreadArr, "arr",arr)
+
+/***deep copy */
+
+let person = {
+  firstname: "pavi",
+  secondName: "k",
+  address: {
+    street: "North",
+    city: " sulur",
+    state: "TN", 
+    country: " India"
+  }
+};
+
+//convert obj to string
+
+let stringObject = JSON.stringify(person);
+console.log(typeof stringObject, stringObject)
+
+let deepClone = JSON.parse(stringObject)
+
+deepClone.secondName = " thran";
+deepClone.address.street = "south" 
+console.log('copiedObject', deepClone)
