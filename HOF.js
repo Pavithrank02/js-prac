ff4f71c9095a2c504c55b43e761de1ad3e4b9c80
Pavithran2 <pavithrank02@gmail.com>
2023-08-11 11:45:42 +0530

    HOF function problems methods and definition

---
+++ HOF.js
@@ -0,0 +1,51 @@
+/**
+ * HOF problems
+ */
+
+let arr = [2,3,4,5]
+//find square of every number
+// for(let i = 0; i< arr.length; i++) {
+//   arr[i] = arr[i] * arr[i]
+ 
+// }
+// //find cube of every number
+// for(let i = 0; i< arr.length; i++) {
+//   arr[i] = arr[i] * arr[i]* arr[i]
+ 
+// }
+// console.log(arr)
+
+function transformer(arr, logic) {
+  let newArr = []
+  for(let i = 0; i< arr.length; i++) {
+      let ans = logic(arr[i])
+      newArr.push(ans)
+     
+    }
+  return newArr
+}
+
+function squarer(elem) {
+  return elem * elem
+}
+function cuber(elem) {
+  return elem * elem * elem
+}
+
+const squredArr  =  transformer(arr, squarer)
+const cubedArr  =  transformer(arr, cuber)
+console.log("cubed", cubedArr)
+console.log("squred", squredArr)
+
+/**
+ * HOF => are the function that accepts functions or returns functions
+ * callbacks => functions that are passed as an argument to another callback functions
+ */
+
+/**
+ * Methods present
+ * foreach
+ * map
+ * filter
+ * reduce
+ */
