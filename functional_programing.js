2bd20e2d9622db6612aca781da327ed28844dc39
Pavithran2 <pavithrank02@gmail.com>
2023-08-11 10:27:22 +0530

    variables and function express

--- functional_programing.js
+++ functional_programing.js
@@ -6,18 +6,37 @@
 
 /**function definition */
 
-function fn() {
-  console.log(" hi")
+// function fn() {
+//   console.log(" hi")
+// }
+
+// /**Fun behaves like objects */
+// fn.count = 0
+
+// fn.printCount = function () {
+//   console.log("count is", this.count)
+// }
+
+// console.log("count prop", fn.count)
+// fn.printCount()
+
+// fn()
+
+/**variable
+ * assign variables as values or reference
+ */
+
+let arr = [1,2,3,4,5]
+
+let arr2 = arr
+
+console.log("arr2", arr2)
+
+/**Function expression */
+
+const funcAddress = function () {
+  console.log("hello im func express")
+
 }
 
-/**Fun behaves like objects */
-fn.count = 0
-
-fn.printCount = function () {
-  console.log("count is", this.count)
-}
-
-console.log("count prop", fn.count)
-fn.printCount()
-
-fn()
+funcAddress()
