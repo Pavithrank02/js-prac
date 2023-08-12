9c70822f85618c22a7ac984ebae2ef60f867a2b0
Pavithran2 <pavithrank02@gmail.com>
2023-08-12 11:08:43 +0530

    nested closure

--- closure.js
+++ closure.js
@@ -1,10 +1,64 @@
-var x = 10
-function b() {
-  console.log("c is the value", x)
+// var x = 10
+// function b() {
+//   console.log("c is the value", x)
+// }
+// function fn() {
+//   var x = 20
+//   b()
+//   console.log("c is the vakue", x)
+// }
+// fn()
+
+/***
+ * closure: inner func forms the closure over the outer functions variable and when the variable are presnt
+ * even if outer scope is removed from the stack
+ * when inner func is hoisted and it forms closure to outer fun
+ */
+
+// function outerFunction () {
+//   let count = 0
+//   function innerFunction() {
+//     count++
+//     return count
+//   }
+//   return innerFunction
+// }
+
+// const innerfn = outerFunction()
+// console.log("first",innerfn())
+// console.log(innerfn())
+// console.log(innerfn())
+
+// function createCounter (a, g) {
+//   function count () {
+//     a = a + g
+//     return a 
+
+//   }
+//   return count
+// }
+
+// let c1= createCounter(10, 5)
+// let c2= createCounter( 5, 2)
+
+// console.log(c1())
+// console.log(c2())
+// console.log(c1())
+// console.log(c2())
+
+function getFirstName(firstname) {
+  console.log("O have got your firstname")
+  return function getLastname(lastname){
+    console.log(" i have got your second name")
+    return function greeting(){
+      console.log(`${firstname} ${lastname}`)
+    }
+  }
 }
-function fn() {
-  var x = 20
-  b()
-  console.log("c is the vakue", x)
-}
-fn()
+
+const fnNmeR = getFirstName("pavi")
+// console.log(fnNmeR)
+
+const laNmeR = fnNmeR("tgf")
+// console.log(laNmeR)
+laNmeR()
