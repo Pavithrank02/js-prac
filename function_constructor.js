8f109234ab47d6bafe063c8506e58bfcc3751f99
Pavithran2 <pavithrank02@gmail.com>
2023-08-14 11:30:54 +0530

    protypsl inheritence

--- function_constructor.js
+++ function_constructor.js
@@ -1,12 +1,21 @@
 /****Function constructor */
 
-function Person(name, age) {
-  this.name = name;
-  this.age = age;
-  this.sayHi = function() {
-    console.log(`jo tjos os ${this.name} and I am ${this.age}`);
-  }
-}
-const jas = Person("jasbin", 26)
-let cap = new Person(" p[avi", 26)
-jas.sayHi()
+// function Person(name, age) {
+//   this.name = name;
+//   this.age = age;
+//   this.sayHi = function() {
+//     console.log(`jo tjos os ${this.name} and I am ${this.age}`);
+//   }
+// }
+// const jas = Person("jasbin", 26)
+// let cap = new Person(" p[avi", 26)
+// jas.sayHi()
+
+/***
+ * prototypal inherience is the process of acquiring the properties of parent
+ * prototype chain
+ */
+let arr = []
+// the chain from current object to null is calledc protype chaim
+console.log(arr.__proto__.__proto__.__proto__)
+
