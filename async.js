c857f7e6bb2a14e96fc161b9ea4d9f8490c9aba6
Pavithran2 <pavithrank02@gmail.com>
2023-08-15 12:04:20 +0530

    continued

--- async.js
+++ async.js
@@ -15,13 +15,13 @@
  * */
 
 
-console.log("before")
+// console.log("before")
 
-function fn() {
-  console.log(" uam fn")
-}
-setTimeout( fn, 2000)
-console.log("after")
+// function fn() {
+//   console.log(" uam fn")
+// }
+// setTimeout( fn, 2000)
+// console.log("after")
 
 /****
  * Environment: Browser
@@ -32,3 +32,18 @@ console.log("after")
  * eventListeners
  * fetch
  */
+// let a = true
+console.log("before")
+
+const cb2 = () => {
+  console.log(" set timeout 1")
+  while (1) {
+
+  }
+}
+const cb1 = () => console.log("hello")
+
+setTimeout(cb2,1000);
+setTimeout(cb1,2000);
+
+console.log("after")
