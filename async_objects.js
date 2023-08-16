a4cad56a50d692d77e0d723b22d346103ac3ddcc
Pavithran2 <pavithrank02@gmail.com>
2023-08-16 09:36:06 +0530

    create polyfil of setInterval

--- async_objects.js
+++ async_objects.js
@@ -15,17 +15,53 @@
 
 
 /****SetInterval */
-console.log("before")
+// console.log("before")
 
-function fn() {
-  console.log("setTimeout callback os called")
+// function fn() {
+//   console.log("setTimeout callback os called")
+// }
+
+// function cancelInterval() {
+//   console.log("cancel inter")
+//   clearInterval(timer)
+// }
+// const timer = setInterval( fn, 1000)
+// setTimeout(cancelInterval, 3000)
+// console.log("after")
+
+/***
+ * create polyfil of setInterval
+ */
+
+function mysetInterval(cb, delay){
+  let obj = {
+    flag : true
+  }
+
+  function myFn(){
+    if(obj.flag == true){
+      cb()
+      setTimeout(myFn, delay)
+    }
+
+  }
+  setTimeout(myFn, delay)
+  return obj
 }
 
-function cancelInterval() {
-  console.log("cancel inter")
-  clearInterval(timer)
+function clearMyInterval(obj) {
+    obj.flag = false
 }
-const timer = setInterval( fn, 1000)
-setTimeout(cancelInterval, 3000)
-console.log("after")
 
+function cb(){
+  console.log('callback')
+}
+
+let obj = mysetInterval(cb, 1000)
+
+function clearCb(){
+  console.log("cancelled the cb")
+  clearMyInterval(obj)
+}
+
+setTimeout(clearCb, 3000)
