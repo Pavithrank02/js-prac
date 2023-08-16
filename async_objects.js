57a235b34f78ae06be227b2de771c9efcf107d83
Pavithran2 <pavithrank02@gmail.com>
2023-08-16 08:59:48 +0530

    setInterval

--- async_objects.js
+++ async_objects.js
@@ -1,14 +1,31 @@
 /****SetTimeout */
 
+// console.log("before")
+
+// function fn() {
+//   console.log("setTimeout callback os called")
+// }
+// const timer = setTimeout( fn, 2000)
+// function canceller(){
+//   console.log("cancelling timeout")
+//   clearTimeout(timer); 
+// }
+// setTimeout(canceller,3000)
+// console.log("after")
+
+
+/****SetInterval */
 console.log("before")
 
 function fn() {
   console.log("setTimeout callback os called")
 }
-const timer = setTimeout( fn, 2000)
-function canceller(){
-  console.log("cancelling timeout")
-  clearTimeout(timer); 
+
+function cancelInterval() {
+  console.log("cancel inter")
+  clearInterval(timer)
 }
-setTimeout(canceller,3000)
+const timer = setInterval( fn, 1000)
+setTimeout(cancelInterval, 3000)
 console.log("after")
+
