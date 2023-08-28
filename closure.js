d0c722b310e43663007797bd784371d787bcd04b
Pavithran2 <pavithrank02@gmail.com>
2023-08-28 19:13:16 +0530

    practice

--- closure.js
+++ closure.js
@@ -81,19 +81,22 @@
 // arrFn[2]()
 // arrFn[2]()
 
-function outer() {
-  let arrFn = []
-  for (let i = 0; i< 3 ; i++) {
-    arrFn.push(function fn(){
-      console.log(i)
-    })
+// function outer() {
+//   let arrFn = []
+//   for (let i = 0; i< 3 ; i++) {
+//     arrFn.push(function fn(){
+//       console.log(i)
+//     })
 
-  }
-  return arrFn
-}
+//   }
+//   return arrFn
+// }
 
-let arrFn = outer()
+// let arrFn = outer()
 
-arrFn[0]()
-arrFn[1]()
-arrFn[2]()
+// arrFn[0]()
+// arrFn[1]()
+// arrFn[2]()
+const num = 2;
+num = 6;
+console.log(num);
