0d2cc7c84e60fb15cace20887959ef7d45dff232
Pavithran2 <pavithrank02@gmail.com>
2023-09-24 11:35:47 +0530

    1

--- callback_promise.js
+++ callback_promise.js
@@ -8,26 +8,32 @@
  * converting cb to promise
  */
 
-Promise.resolve(1)
-.then((val) => {
-  console.log(val)
-  return val + 1
-}).then((val) => {
-  console.log(val)
-}).then((val) => {
-  console.log(val)
-  return Promise.resolve(3)
-    .then((val) => {
-      console.log(val)
-    })
-}).then((val) => {
-  console.log(val)
-  return Promise.reject(4)
-}).catch((val) => {
-  console.log(val)
-}).finally((val) => {
-  console.log(val)
-  return 10
-}).then((val) => {
-  console.log(val)
-})
+// Promise.resolve(1)
+// .then((val) => {
+//   console.log(val)
+//   return val + 1
+// }).then((val) => {
+//   console.log(val)
+// }).then((val) => {
+//   console.log(val)
+//   return Promise.resolve(3)
+//     .then((val) => {
+//       console.log(val)
+//     })
+// }).then((val) => {
+//   console.log(val)
+//   return Promise.reject(4)
+// }).catch((val) => {
+//   console.log(val)
+// }).finally((val) => {
+//   console.log(val)
+//   return 10
+// }).then((val) => {
+//   console.log(val)
+// })
+for (var i = 0; i < 5; i++) {
+  setTimeout(() => console.log(i), 0)
+}
+for (let i = 0; i < 5; i++) {
+  setTimeout(() => console.log(i), 0)
+}
