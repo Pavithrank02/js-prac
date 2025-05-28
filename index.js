cbe7fbb82b551d0a836ef959d107e4a30667d382
Pavithran2 <pavithrank02@gmail.com>
2025-05-28 20:59:06 +0530

    rt

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-var element = document.getElementById("myDiv")
-console.log(element.textContent)
+const element = document.getElementById("myDiv");
+
+// Using setAttribute
+element.setAttribute("id", "newId");
+
+const element1 = document.createElement("div")
+element1.textContent = " We Ar"
+document.body.append(element1)
+
+const newDiv = document.createElement("div");
+newDiv.textContent = "Hello, World!";
+document.body.appendChild(newDiv);
+
+// Using direct property
+element.id = "newId";
+
+console.log(element)
