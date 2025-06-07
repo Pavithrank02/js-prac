61cc0ef5d4f4566486b8d3b397dd9a79f18a6d55
Pavithran2 <pavithrank02@gmail.com>
2025-06-07 11:50:32 +0530

    we

--- index.js
+++ index.js
@@ -1,17 +1,4 @@
-const element = document.getElementById("myDiv");
+let a = 0
+let b = 8
 
-// Using setAttribute
-element.setAttribute("id", "newId");
-
-const element1 = document.createElement("div")
-element1.innerHTML = " We Ar"
-document.body.append(element1)
-
-const newDiv = document.createElement("div");
-newDiv.textContent = "Hello, World!";
-document.body.appendChild(newDiv);
-
-// Using direct property
-element.id = "newId";
-
-console.log(element)
+console.log(eval(a, b))
